acfc1eda892e490204358d8141a3f586c7018846
Cyril BOCHET <bochet66@gmail.com>
2023-03-01 02:16:52 +0100

    wip

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import './scripts/i18next/i18next-conf';
-import './scripts/navbar';
 import './scripts/main';
 
 
+import './css/metropolis.css';
 import './css/styles.css';
 import './css/boostrap.css';
 import './css/media-queries.css';
--- src/scripts/navbar.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#navbar").addClass("navbar-shrink");
-    } else {
-        $("#navbar").removeClass("navbar-shrink");
-    }
-}
