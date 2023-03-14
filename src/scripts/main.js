96d4407f8a8b48d902450f2e34d3efd0af491ec5
Cyril BOCHET <bochet66@gmail.com>
2023-03-14 01:12:08 +0100

    image to webp + new minia + new projects + clean images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,30 +25,36 @@ function getAge(dateString) {
 
 //Filter portfolio
 
-loadJSon()
+let projects;
+$.getJSON('dist/docs/projects.json', function (projectsJson) {
+    projects = projectsJson.sort(function () {
+        return 0.5 - Math.random()
+    });
+        processJson();
 
-function loadJSon() {
-    $.getJSON('dist/docs/projects.json', function (projects) {
-        $(document).ready(function () {
-            $('#age').html(getAge('1998/10/22'));
-            $('#year').html(new Date().getFullYear());
+});
 
-            const projectsGallery = $('#projects-gallery');
-            projectsGallery.empty();
-            let btnDetailsText = $('#btn-details-text').text();
+function processJson() {
+    $(document).ready(function () {
+        $('#age').html(getAge('1998/10/22'));
+        $('#year').html(new Date().getFullYear());
 
-            let langPreference = localStorage.getItem('i18nextLng');
+        const projectsGallery = $('#projects-gallery');
+        projectsGallery.empty();
+        let btnDetailsText = $('#btn-details-text').text();
 
-            const lang = $('#language-selector-value').text();
-            let titleKey = `titre-${lang}`;
+        let langPreference = localStorage.getItem('i18nextLng');
 
-            if (langPreference !== undefined) {
-                titleKey = `titre-${langPreference}`;
-            }
+        const lang = $('#language-selector-value').text();
+        let titleKey = `titre-${lang}`;
 
-            for (let i = 0; i < projects.length; i++) {
+        if (langPreference !== undefined) {
+            titleKey = `titre-${langPreference}`;
+        }
 
-                const card = ` <div class="col-lg-4 col-md-6 col-sm-6 col-12 mb-5" data-project-type="${projects[i]["type"]}">
+        for (let i = 0; i < projects.length; i++) {
+
+            const card = ` <div class="col-lg-4 col-md-6 col-sm-6 col-12 mb-5" data-project-type="${projects[i]["type"]}">
                         <div class="card">
                             <div class="card-body p-0 surface">
                                 <img class="img-fluid card-img"
@@ -59,69 +65,67 @@ function loadJSon() {
                             </div>
                         </div>
                     </div>`;
-                const cardElement = $(card);
-                cardElement.find('div.card-body.on-surface-text').append($('<a>').addClass('btn btn-tertiary')
-                    .attr('data-project-id', projects[i]["id"])
-                    .attr('data-bs-toggle', 'offcanvas')
-                    .attr('data-bs-target', '#offcanvasProject')
-                    .attr('aria-controls', 'offcanvasProject')
-                    .attr('data-i18n', 'form.btn.details')
-                    .text(btnDetailsText)
-                    .click(function (event) {
-                        getProject(event, projects[i]["id"]);
-                    }));
-                $(projectsGallery).append(cardElement);
-            }
-
-
-        });
-
-        function getProject(e, projectId) {
-            const project = projects.find(p => p.id === projectId);
-            if (!project) return;
-
-            let langPreference = localStorage.getItem('i18nextLng');
-            const lang = $('#language-selector-value').text();
-
-            let descriptionKey = `description-${lang}`;
-            let titleKey = `titre-${lang}`;
-
-            if (langPreference !== undefined) {
-                titleKey = `titre-${langPreference}`;
-                descriptionKey = `description-${langPreference}`;
-            }
-
-            const description = project[descriptionKey];
-            const titre = project[titleKey];
-            const tag = project["tag"];
-            const images = project["images"];
-
-            $('#project-description').html(description);
-            $('#project-title').text(titre);
-            $('#project-tags').empty();
-            $('#project-images').empty();
-
-            const tagsHtml = tag.map(t => `<span class="badge badge-project me-2">#${t}</span>`).join('');
-            $('#project-tags').html(tagsHtml);
-
-            const imagesHtml = images.map((img, index) => `
-        <a href="dist/images/projets/illustrations/${img}">
-            <img class="img-fluid img-project" src="dist/images/projets/illustrations/${img}" data-fancybox="project-${projectId}" data-caption="${titre} - ${index + 1}">
-        </a>
-    `).join('');
-            $('#project-images').html(imagesHtml);
-            Fancybox.bind('[data-fancybox=project-' + projectId + ']', {});
+            const cardElement = $(card);
+            cardElement.find('div.card-body.on-surface-text').append($('<a>').addClass('btn btn-tertiary')
+                .attr('data-project-id', projects[i]["id"])
+                .attr('data-bs-toggle', 'offcanvas')
+                .attr('data-bs-target', '#offcanvasProject')
+                .attr('aria-controls', 'offcanvasProject')
+                .attr('data-i18n', 'form.btn.details')
+                .text(btnDetailsText)
+                .click(function (event) {
+                    getProject(event, projects[i]["id"]);
+                }));
+            $(projectsGallery).append(cardElement);
         }
 
 
     });
+
+    function getProject(e, projectId) {
+        const project = projects.find(p => p.id === projectId);
+        if (!project) return;
+
+        let langPreference = localStorage.getItem('i18nextLng');
+        const lang = $('#language-selector-value').text();
+
+        let descriptionKey = `description-${lang}`;
+        let titleKey = `titre-${lang}`;
+
+        if (langPreference !== undefined) {
+            titleKey = `titre-${langPreference}`;
+            descriptionKey = `description-${langPreference}`;
+        }
+
+        const description = project[descriptionKey];
+        const titre = project[titleKey];
+        const tag = project["tag"];
+        const images = project["images"];
+
+        $('#project-description').html(description);
+        $('#project-title').text(titre);
+        $('#project-tags').empty();
+        $('#project-images').empty();
+
+        const tagsHtml = tag.map(t => `<span class="badge badge-project me-2">#${t}</span>`).join('');
+        $('#project-tags').html(tagsHtml);
+
+        const imagesHtml = images.map((img, index) => `
+        <a href="dist/images/projets/illustrations/${img}">
+            <img class="img-fluid img-project" src="dist/images/projets/illustrations/${img}" data-fancybox="project-${projectId}" data-caption="${titre} - ${index + 1}">
+        </a>
+    `).join('');
+        $('#project-images').html(imagesHtml);
+        Fancybox.bind('[data-fancybox=project-' + projectId + ']', {});
+    }
+
 }
 
 
 $(document).ready(function () {
     $('#language-selector-menu button, #language-selector-menu-mobile button').on('click', function () {
         $('#projects-gallery').html();
-        loadJSon()
+        processJson();
     });
 
     $("#navbarCollapse .navbar-nav .nav-item, #language-selector-menu-mobile .dropdown-item").on('click', function () {
@@ -129,9 +133,9 @@ $(document).ready(function () {
     });
 
     var lastScrollTop = 0;
-    window.addEventListener("scroll", function(){
+    window.addEventListener("scroll", function () {
         var currentScrollTop = window.scrollY || document.documentElement.scrollTop;
-        if (currentScrollTop > lastScrollTop){
+        if (currentScrollTop > lastScrollTop) {
             document.querySelector(".navbar").classList.add("navbar-hidden");
         } else {
             document.querySelector(".navbar").classList.remove("navbar-hidden");
